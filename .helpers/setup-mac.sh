b26cbb9aa08b6d9930bee5e809e3c9c96b6a59c6
Michael Nowak <michael@nowakphd.com>
2021-08-29 10:33:27 -0500

    updated version to latest

--- .helpers/setup-mac.sh
+++ .helpers/setup-mac.sh
@@ -1,3 +1,3 @@
 export PATH_TO_CS128_DIR=$"$(pwd)" # this should be root cs128env
 echo "Path to cs128env folder: $PATH_TO_CS128_DIR"
-docker run -d -v "$PATH_TO_CS128_DIR/src:/home/vagrant/src" -v "$PATH_TO_CS128_DIR/.shared:/home/vagrant/.shared" -p 2222:22 --security-opt seccomp=unconfined --name cs128env michaelrnowak/cpp-dev-env:v11
+docker run -d -v "$PATH_TO_CS128_DIR/src:/home/vagrant/src" -v "$PATH_TO_CS128_DIR/.shared:/home/vagrant/.shared" -p 2222:22 --security-opt seccomp=unconfined --name cs128env michaelrnowak/cpp-dev-env:latest
