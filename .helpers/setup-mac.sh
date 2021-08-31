5391a3cddbed0496d4f815a55d2af07eda9c4b92
Michael Nowak <michael@nowakphd.com>
2021-08-31 17:20:28 -0500

    updated mistake

--- .helpers/setup-mac.sh
+++ .helpers/setup-mac.sh
@@ -1,8 +1,8 @@
 export PATH_TO_CS128_DIR=$"$(pwd)" # this should be root cs128env
 echo "Path to cs128env folder: $PATH_TO_CS128_DIR"
 if [[ `uname -m` == 'arm64' ]]; then
-  docker pull michaelrnowak/cpp-dev-env:latest-m1
-  docker run -d -v "$PATH_TO_CS128_DIR/src:/home/vagrant/src" -v "$PATH_TO_CS128_DIR/.shared:/home/vagrant/.shared" -p 2222:22 --security-opt seccomp=unconfined --name cs128env michaelrnowak/cpp-dev-env:latest-m1
+  docker pull michaelrnowak/cpp-dev-env:m1-latest
+  docker run -d -v "$PATH_TO_CS128_DIR/src:/home/vagrant/src" -v "$PATH_TO_CS128_DIR/.shared:/home/vagrant/.shared" -p 2222:22 --security-opt seccomp=unconfined --name cs128env michaelrnowak/cpp-dev-env:m1-latest
 else
   docker pull michaelrnowak/cpp-dev-env:latest
   docker run -d -v "$PATH_TO_CS128_DIR/src:/home/vagrant/src" -v "$PATH_TO_CS128_DIR/.shared:/home/vagrant/.shared" -p 2222:22 --security-opt seccomp=unconfined --name cs128env michaelrnowak/cpp-dev-env:latest
