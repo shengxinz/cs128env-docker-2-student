7b825b0491c9ed75189defcb0e19ec1b64385cd3
Michael Nowak <michael@nowakphd.com>
2022-01-21 17:14:03 -0600

    nvm

--- .helpers/setup-mac.sh
+++ .helpers/setup-mac.sh
@@ -9,10 +9,6 @@ else
     echo "Running on Apple Silcon with Rosetta 2"
     docker pull michaelrnowak/cpp-dev-env:m1-latest
     docker run -d -v "$PATH_TO_CS128_DIR/src:/home/vagrant/src" -v "$PATH_TO_CS128_DIR/.shared:/home/vagrant/.shared" -p 2222:22 --security-opt seccomp=unconfined --name cs128env michaelrnowak/cpp-dev-env:m1-latest
-  elif [ "$(sysctl -in sysctl.proc_translated)" = "0" ]; then
-    echo "Running on native Apple Silcon"
-    docker pull michaelrnowak/cpp-dev-env:m1-latest
-    docker run -d -v "$PATH_TO_CS128_DIR/src:/home/vagrant/src" -v "$PATH_TO_CS128_DIR/.shared:/home/vagrant/.shared" -p 2222:22 --security-opt seccomp=unconfined --name cs128env michaelrnowak/cpp-dev-env:m1-latest
   else
     echo "Running on native Intel"
     docker pull michaelrnowak/cpp-dev-env:latest
