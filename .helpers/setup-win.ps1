effe8dfc14c507e67ba4090eb0dd583b2c63d520
Nathan Park <47864613+natepark01@users.noreply.github.com>
2022-01-30 16:17:56 -0600

    Correct syntax

--- .helpers/setup-win.ps1
+++ .helpers/setup-win.ps1
@@ -9,7 +9,7 @@ if ((Test-Admin) -eq $false)  {
     if ($elevated) {
         # tried to elevate, did not work, aborting
     } else {
-        Start-Process powershell.exe -WorkingDirectory PSScriptRoot -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+        Start-Process powershell.exe -WorkingDirectory $PSScriptRoot -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
     }
     exit
 }
